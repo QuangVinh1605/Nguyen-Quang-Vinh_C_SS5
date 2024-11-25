de75f6408c752a44a4c874eebe967adac1e908dd
QuangVinh1605 <nhv16052006@gmail.com>
2024-11-25 23:22:54 +0700

    Add files via upload

---
+++ C_SS5_01.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int n=5;
+	int i;
+	printf("moi ban nhap vao mot so nguyen:",i);
+	scanf("%d",&i);
+	while(i!=5){
+		printf("moi ban nhap vao mot so nguyen:",i);
+		scanf("%d,",&i);
+	}
+} 
